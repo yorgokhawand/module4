4e965767337dbacf5fe61b858fc4b1a3ff93d88e
MBP <mbp@MBPs-MacBook-Pro.local>
2020-12-05 16:29:45 +0200

    initial commit

---
+++ goodbye.js
@@ -0,0 +1,14 @@
+(function(window) {
+	var speakWord = "Good Bye";
+	var byeSpeaker = function (name) {
+  		console.log(speakWord + " " + name);
+	}
+	
+	window.byeSpeaker = byeSpeaker;
+
+})(window);
+
+
+
+
+
---
+++ hello.js
@@ -0,0 +1,14 @@
+(function(window) {
+	var speakWord = "Hello";
+	var helloSpeaker = function (name) {
+		console.log(speakWord + " " + name);
+	}
+
+	window.helloSpeaker = helloSpeaker;
+
+})(window);
+
+
+
+
+
